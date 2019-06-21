7ae8f9538f7db6f9a6b4e953fadfb715b0d9608f
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2019-06-21 21:59:53 +0530

    fix git pull and push shortcuts

--- git/git.sh
+++ git/git.sh
@@ -4,10 +4,10 @@
 gitremote=origin
 
 # git pull current branch
-alias gplc="git pull $gitremote $(git branch | awk '/*/ {print $2}')"
+alias gplc="git pull $gitremote \$(git branch | awk '/*/ {print \$2}')"
 
 # git push current branch 
-alias gphc="git push $gitremote $(git branch | awk '/*/ {print $2}')"
+alias gphc="git push $gitremote \$(git branch | awk '/*/ {print \$2}')"
 
 # git commit with gpg keys with different keys
 alias gco="git commit -SGPG_KEY_OFFICE"
