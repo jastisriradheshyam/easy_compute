35a4bca7b2538c55cfd4352c8b2f4e07191b8b1b
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2021-01-06 19:44:45 +0000

    add git list files command
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- git/git.sh
+++ git/git.sh
@@ -9,6 +9,9 @@ alias gplc="git pull $gitremote \$(git branch | awk '/*/ {print \$2}')"
 # git push current branch 
 alias gphc="git push $gitremote \$(git branch | awk '/*/ {print \$2}')"
 
+# git files affected in perticular commit
+alias gchf_commit="git diff-tree --no-commit-id --name-only -r"
+
 # git commit with gpg keys with different keys
 alias gco="git commit -SGPG_KEY_OFFICE"
 alias gcp="git commit -SGPG_KEY_PERSONAL"
---
+++ git/readme.md
@@ -0,0 +1,9 @@
+# GIT
+
+### List all files in particular commit
+- `git diff-tree --no-commit-id --name-only -r COMMIT_ID`
+   - it's a plumbing command
+- `git show --pretty="" --name-only COMMIT_ID`
+   - it's a porcelain command
+- reference: https://stackoverflow.com/questions/424071/how-to-list-all-the-files-in-a-commit
+
