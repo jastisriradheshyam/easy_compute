d303db571265e3fdfd85ce46567863771d2dfdee
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2021-05-05 11:40:44 +0000

    fix public key content color in ssh key gen
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- ssh/keygen_host.sh
+++ ssh/keygen_host.sh
@@ -58,8 +58,12 @@ function generate() {
     chmod 400 ~/.ssh/$host.pem
 }
 
+function generate_public_key_path() {
+    public_key_path=~/Documents/ssh_public/$host.public_key.txt
+}
+
 function move_public_key() {
-    yes | mv ~/.ssh/$host.pem.pub ~/Documents/ssh_public/$host.public_key.txt
+    yes | mv ~/.ssh/$host.pem.pub $public_key_path
 }
 
 function set_config() {
@@ -72,13 +76,17 @@ function set_config() {
 }
 
 function show_public_key() {
-    echo -e "\e[32m[+]\e[39m Public key Content Is:\n\e[90m"
-    cat ~/Documents/ssh_public/$host.public_key.txt
+    echo -e "\e[32m[+]\e[39m Public key file path: $public_key_path\n"
+    echo -e "\e[32m[+]\e[39m Public key content Is:\n\e[40m\e[34m"
+    # directly using cat caused a new line after the echo and background color leaked through the echo
+    local public_key=$(cat "$public_key_path")
+    echo -e "${public_key}\e[0m\n"
 }
 
 check
 bootstap
 generate
+generate_public_key_path
 move_public_key
 set_config
 show_public_key
--- ssh/keygen_host_github.sh
+++ ssh/keygen_host_github.sh
@@ -6,14 +6,16 @@ echo -e "\e[34m[/]\e[39m Generating SSH Key For Github..."
 mkdir ~/.ssh ~/Documents &> /dev/null && chmod 700 ~/.ssh
 rm -f ~/.ssh/*github.pem
 ssh-keygen -t rsa -N "" -b 4096 -f ~/.ssh/github.pem > /dev/null
-yes | mv ~/.ssh/github.pem.pub ~/Documents/github.public_key.txt
+yes | mv ~/.ssh/github.pem.pub ~/Documents/ssh_public/github.public_key.txt
 chmod 400 ~/.ssh/github.pem
 text="\n## Github ##\nHost github.com\n IdentityFile ~/.ssh/github.pem\n"
 echo -e "$text" >> ~/.ssh/config
 chmod 600 ~/.ssh/config
 echo -e "\e[34m[/]\e[39m Generated Public Key Saved To: [\e[35m ~/Documents/github.public_key.txt \e[39m]"
-echo -e "\e[32m[+]\e[39m Public key Content Is:\n\e[90m"
-cat ~/Documents/github.public_key.txt
+echo -e "\e[32m[+]\e[39m Public key Content Is:\n\e[40m\e[34m"
+# directly using cat caused a new line after the echo and background color leaked through the echo
+local public_key=$(cat ~/Documents/ssh_public/github.public_key.txt)
+echo -e "${public_key}\e[0m\n"
 echo -e "\n\e[32m[+]\e[39m Paste This Public Key Content To: [\e[36m https://github.com/settings/keys \e[39m]"
 
 # ----------------------------------------------------------------------------------- #
