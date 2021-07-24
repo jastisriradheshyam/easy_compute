255d750176009982cd6eea8c7b5a1a343206fd1b
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2021-07-24 06:47:17 +0000

    fix mawk git current branch issue - git current branch in pull alias was not working with mawk - added few other parameters to git commit and signing
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- git/git.sh
+++ git/git.sh
@@ -3,15 +3,21 @@
 # git remote
 gitremote=origin
 
+# git current branch (works on both gawk and mawk)
+alias git_cur_branch="git branch | awk '/^\\*/{print \$2}'"
+
 # git pull current branch
-alias gplc="git pull $gitremote \$(git branch | awk '/*/ {print \$2}')"
+alias gplc="git pull $gitremote \$(git_cur_branch)"
 
 # git push current branch 
-alias gphc="git push $gitremote \$(git branch | awk '/*/ {print \$2}')"
+alias gphc="git push $gitremote \$(git_cur_branch)"
 
 # git files affected in perticular commit
 alias gchf_commit="git diff-tree --no-commit-id --name-only -r"
 
 # git commit with gpg keys with different keys
-alias gco="git commit -SGPG_KEY_OFFICE"
-alias gcp="git commit -SGPG_KEY_PERSONAL"
+alias gc_office="git commit -s -SGPG_KEY_OFFICE"
+alias gc_personal="git commit -s -SGPG_KEY_PERSONAL"
+
+# enable if force gpg passphrase retrival from terminal
+# export GPG_TTY=$(tty)
