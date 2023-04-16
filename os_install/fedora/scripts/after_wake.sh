d4a246e15c732fdee8b286e0c7232dc420f84175
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2023-04-16 15:26:18 +0530

    add after wake up script (some bug in fedora, after wake up from sleep it does not restore to previous power state, makes the system slugish)
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

---
+++ os_install/fedora/scripts/after_wake.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo cpupower idle-set -d 3
+sleep 3
+sudo cpupower idle-set -d 2
+sleep 3
+echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+sleep 3
+sudo systemctl restart thermald.service
