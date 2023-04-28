8b6aabd77e6eff28758e164c9604ca4bd8fb196d
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2023-04-28 09:50:34 +0530

    update after_wake script
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- os_install/fedora/scripts/after_wake.sh
+++ os_install/fedora/scripts/after_wake.sh
@@ -6,4 +6,9 @@
 #sleep 3
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 sleep 3
+echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost
 sudo systemctl restart thermald.service
+
+# References
+# https://docs.kernel.org/admin-guide/pm/intel_pstate.html
