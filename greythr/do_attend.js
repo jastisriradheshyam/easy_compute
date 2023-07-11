323cf0df64a72ff21b111ceb73fa8b3ed28be132
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2023-07-11 13:13:02 +0530

    add check swipes to greythr
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- greythr/do_attend.js
+++ greythr/do_attend.js
@@ -17,13 +17,26 @@ const doSignIn = async (JSESSIONID, PLAY_SESSION, access_token) => {
   console.log((response.headers.get('content-type').includes('json')) ? await response.json() : await response.text());
 }
 
+const checkSwipes = async (JSESSIONID, PLAY_SESSION, access_token) => {
+  const response = await fetch(`https://${creds.companyCode}.greythr.com/v3/api/attendance/swipes`, {
+    method: 'GET',
+    headers:
+    {
+      'accept': 'application/json',
+      'cookie': `JSESSIONID=${JSESSIONID}; access_token=${access_token}; PLAY_SESSION=${PLAY_SESSION}`,
+    },
+  });
+  console.log(response.status);
+  console.log(response.headers);
+  console.log((response.headers.get('content-type').includes('json')) ? await response.json() : await response.text());
+}
 
 const do_attend = async () => {
   const {
     JSESSIONID, PLAY_SESSION, access_token
   } = await getCreds();
   await doSignIn(JSESSIONID, PLAY_SESSION, access_token)
+  await checkSwipes(JSESSIONID, PLAY_SESSION, access_token)
 }
 
 do_attend()
-
